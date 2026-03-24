4c4c361d43c36932da6b0bb9d52f27b9021ca9f2
MaksymHolovniak <maksum.golovnyak@gmail.com>
2026-03-24 13:00:22 +0200

    Configured workspace

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  eslintConfigPrettier,
+]);
